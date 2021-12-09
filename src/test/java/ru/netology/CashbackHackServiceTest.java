e596606444e08baae66badf875faa518cd2f11d8
Vladimir Kovalev <boban_envorter@mail.ru>
2021-12-09 19:54:27 +0300

    first test

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,4 +1,19 @@
-import static org.testng.Assert.*;
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
-    
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldService0() {
+
+        assertEquals(service.remain(0), 1000);
+
+
+
+    }
 }
