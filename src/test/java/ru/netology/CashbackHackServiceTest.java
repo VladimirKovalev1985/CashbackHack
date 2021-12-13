690a5a8c2aa49fe46f6684784dbec1a94633955b
Vladimir Kovalev <boban_envorter@mail.ru>
2021-12-13 19:39:16 +0300

    add tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -12,8 +12,26 @@ public class CashbackHackServiceTest {
     public void shouldService0() {
 
         assertEquals(service.remain(0), 1000);
+    }
 
+    @Test
+    public void shouldService1000() {
 
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldServiceMore1000() {
+
+        assertEquals(service.remain(1100), 900);
+    }
+
+    @Test
+    public void shouldServiceLess1000() {
+
+        assertEquals(service.remain(600), 400);
 
     }
+
+
 }
